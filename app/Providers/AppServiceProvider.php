221e01149bf5a8c0f6869a78e58e5681234b2151
irimajiri <irimajiri.satoko@gmail.com>
2021-11-26 15:41:33 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         
+        
         if(\App::environment(['production'])){
         \URL::forceScheme('https');
         }
