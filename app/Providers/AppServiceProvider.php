ee8e6203874cc0bc3f333584a86e070cf9fa4888
irimajiri <irimajiri.satoko@gmail.com>
2021-11-26 15:12:55 +0900

    変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
         
-    // if(\App::environment(['production'])){
-    //     \URL::forseScheme('https');
-    //  }
+    if(\App::environment(['production'])){
+        \URL::forseScheme('https');
+     }
      
     }
 }
